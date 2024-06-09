205f2f02911c8812c1ea140369d22fe0f04b2961
Yogeshwar Gupta <1sandeepjha2@gmail.com>
2024-06-09 19:34:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import TicTacToe from './Components/TicTacToe/TicTacToe'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
